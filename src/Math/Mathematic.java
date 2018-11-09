3e0cf25f34334cf8ddd0227777d24ae6c64ceac3
Mike Ludemann <mike-ludemann@hotmail.de>
2018-11-09 17:54:53 +0100

    Updating MAth directory and files

--- src/Math/Mathematic.java
+++ src/Math/Mathematic.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-public class ppercentage {
+public class Mathematic {
 
     public static double percentage(double value, double from) {
         double percentage = (value * 100) / from;
         return percentage;
     }
+
 }

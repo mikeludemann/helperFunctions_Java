9b63c8dec81c2170efc3e340d709109fa536bb04
Mike Ludemann <mike-ludemann@hotmail.de>
2018-11-09 16:24:41 +0100

    Starting with Math functions

---
+++ src/Math/percentage.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ppercentage {
+
+    public static double percentage(double value, double from) {
+        double percentage = (value * 100) / from;
+        return percentage;
+    }
+}
